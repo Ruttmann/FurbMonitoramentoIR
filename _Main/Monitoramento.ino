ba958d9c221d374be385d92aa234e3f9d74514cd
Ruttmann <m.ruttmann@hotmail.com>
2018-06-25 22:06:50 -0300

    Altera desligaDispositivos(): - Tenta desligar dispositivo no máximo 3 vezes. Se não desligar, emite ao servidor uma mensagem de falha.

--- _Main/Monitoramento.ino
+++ _Main/Monitoramento.ino
@@ -69,21 +69,34 @@ bool projetorEstaLigado() {
   }
 }
 
-void desligaDispositivos() { 
-  if (arEstaLigado()) {
-    Serial.println(F("Desligando ar-condicionado..."));
-    sendRawSignal(true);
+void desligaDispositivos() {
+  for (int i=0; i<3; i++) {
+    if (arEstaLigado()) {
+      Serial.println(F("Desligando ar-condicionado..."));
+      sendRawSignal(true);
+      delay(1000);
+    }
   }
 
-  delay(5000);
+  if (arEstaLigado()) {
+    enviarMensagem("failure", "id", "ar");
+  }
+  
+  delay(2000);
+
+  for (int i=0; i<3; i++) {
+    if (projetorEstaLigado()) {
+      Serial.println(F("Desligando projetor..."));
+      sendRawSignal(false);
+      delay(2000);
+      if (projetorEstaLigado()) {
+        Serial.println(F("Enviado segundo sinal ao projetor..."));
+        sendRawSignal(false);
+      }
+    }
+  }
 
   if (projetorEstaLigado()) {
-    Serial.println(F("Desligando projetor..."));
-    sendRawSignal(false);
-    delay(3000);
-    if (projetorEstaLigado()) {
-      Serial.println(F("Enviado segundo sinal ao projetor..."));
-      sendRawSignal(false);
-    }
+    enviarMensagem("failure", "id", "pj");
   }
 }
